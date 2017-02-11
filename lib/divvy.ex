472a03c15da6f5e8870896a453b6965a88c06a27
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-02-11 06:28:50 -0600

    Basic function to fetch Divvy feed

--- lib/divvy.ex
+++ lib/divvy.ex
@@ -1,2 +1,6 @@
 defmodule Divvy do
+  def feed do
+    %HTTPoison.Response{body: body, status_code: 200} = HTTPoison.get! "https://feeds.divvybikes.com/stations/stations.json"
+    Poison.Parser.parse! body
+  end
 end
