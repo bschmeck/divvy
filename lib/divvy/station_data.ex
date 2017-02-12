0554dfa0d4370d7315aff57cc1ff39cb6e59d15e
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-02-11 22:18:59 -0600

    Switch to a Client/Server API for Divvy.StationData

--- lib/divvy/station_data.ex
+++ lib/divvy/station_data.ex
@@ -1,6 +1,22 @@
 defmodule Divvy.StationData do
   use GenServer
 
+  # Client
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def for_station(station_id) do
+    GenServer.call(__MODULE__, {:for, station_id})
+  end
+
+  def update(data) do
+    GenServer.cast(__MODULE__, {:update, data})
+  end
+
+  # Server
+
   def handle_call({:for, station_id}, _from, station_data) do
     reply = case Enum.find(station_data, &(&1["id"] == station_id)) do
               nil -> {:missing, %{}}
