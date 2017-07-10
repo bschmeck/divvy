f28da116729cb70934b67975d97f6798ea75dfc8
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-07-10 08:29:16 -0500

    Control output with configuration GenServer instead of command line args

--- output.exs
+++ output.exs
@@ -1,6 +1,3 @@
-[station_name, station_id] = System.argv
-{station_id, _} = Integer.parse(station_id)
-
 printer_maker = fn(name) ->
   fn(%{"availableBikes" => bikes, "availableDocks" => docks, "totalDocks" => capacity}) ->
     reds = capacity - bikes - docks
@@ -8,6 +5,8 @@ printer_maker = fn(name) ->
   end
 end
 
+{:ok, station_id} = GenServer.call({Divvy.Configuration, :"divvy@osprey"}, {:get_primary})
+{:ok, station_name} = GenServer.call({Divvy.Configuration, :"divvy@osprey"}, {:alias_for, station_id})
 printer = printer_maker.(station_name)
 
 case GenServer.call({Divvy.StationData, :"divvy@osprey"}, {:get, station_id}) do
